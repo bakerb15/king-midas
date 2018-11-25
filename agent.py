184b641c42cd7aa6efe0b88e72216fa2972b3a87
Bradley J. Baker <bakerb15@msu.edu>
2018-11-24 22:36:29 -0500

    Missing some market data for a few companies. Simulation working

--- agent.py
+++ agent.py
@@ -1,10 +1,40 @@
+from collections import deque
 
 class Agent(object):
 
-    def __init__(self, starting_dollars, market_simulation):
+    def __init__(self, agent_name, starting_dollars, market_simulation):
+        self.name = agent_name
         self.dollars = starting_dollars
         self.market_simulation = market_simulation
         self.portfolio = {}
-        self.knowledge = {}
+        self.knowledge = deque()
+        self.MAX_KNOWLEDGE = 500
+        self.last_company_price = {}
+        self.last_market_index_price = {}
+
+    def observe(self, date, info):
+        if len(self.knowledge) < self.MAX_KNOWLEDGE:
+            self.knowledge.append((date, info))
+        else:
+            self.knowledge.popleft()
+            self.knowledge.append((date, info))
+        for key in info:
+            if key in self.market_simulation.companies:
+                self.last_company_price[key] = info[key]['Close']
+            elif key in self.market_simulation.market_indices:
+                self.last_market_index_price[key] = info[key]['Close']
+
+    def act(self):
+        pass
+
+    def calculate_net_worth(self):
+        nw = self.dollars
+        for stock in self.portfolio:
+            nw += self.last_company_price[stock]
+        return nw
+
+
+
+
 
 
--- market.py
+++ market.py
@@ -3,17 +3,21 @@ import os
 from enum import Enum
 import pandas
 import datetime
+from collections import OrderedDict
+from agent import Agent
 
 class Market(object):
 
     def __init__(self, transaction_cost):
-        self.companies = {}
-        self.market_indices = {}
+        self.companies = OrderedDict()
+        self.market_indices = OrderedDict()
         self.transaction_cost = transaction_cost
         for key in company:
             self.companies[company[key]] = {'name' : key}
         for key in market_index:
             self.market_indices[market_index[key]] = {'name' : key}
+        sorted(self.companies)
+        sorted(self.market_indices)
         data_dir = './data'
         for file_name in os.listdir(data_dir):
             if '.csv' in file_name:
@@ -21,10 +25,11 @@ class Market(object):
                 symbol = file_name.split('.csv')[0]
                 if symbol in self.companies:
                     print('Loading {}'.format(self.companies[symbol]['name']))
-                    self.companies[symbol]['data'] = pandas.read_csv(path_to_file)
+                    self.companies[symbol]['data'] = pandas.read_csv(path_to_file, index_col=0)
+
                 elif symbol in self.market_indices:
                     print('Loading {}'.format(self.market_indices[symbol]['name']))
-                    self.market_indices[symbol]['data'] = pandas.read_csv(path_to_file)
+                    self.market_indices[symbol]['data'] = pandas.read_csv(path_to_file, index_col=0)
                 else:
                     print('{} is not in market_data dictionaries.'.format(symbol))
 
@@ -34,15 +39,30 @@ class Market(object):
         if length.days > 0:
             day = start_date
             while day < end_date:
-                print(day.strftime('%Y-%m-%d %a'))
+                info = {}
+                date = day.strftime('%Y-%m-%d')
+                for ind in self.market_indices:
+                    if date in self.market_indices[ind]['data'].index:
+                        info[ind] = self.market_indices[ind]['data'].loc[date]
+                for co in self.companies:
+                    try:
+                        if date in self.companies[co]['data'].index:
+                                info[co] = self.companies[co]['data'].loc[date]
+                    except KeyError:
+                        break;
+                for ag in agents:
+                    ag.observe(day, info)
+                for ag in agents:
+                    print('{}\t{}\t{}'.format(date, ag.name, ag.calculate_net_worth()))
                 day += datetime.timedelta(days=1)
 
 if __name__ == '__main__':
     TRANSACTION_COST = 10.0
     print('Creating market')
-    market = Market(TRANSACTION_COST)
+    mark = Market(TRANSACTION_COST)
     print('Market data loaded')
-    whole_start = datetime.date(2008, 11, 1)
-    whole_end = datetime.date(2008, 12, 12)
-    market.simulate(whole_start, whole_end)
+    whole_start = datetime.date(2009, 11, 3)
+    whole_end = datetime.date(2012, 12, 12)
+    agents = [Agent('sally', 100000.0, mark)]
+    mark.simulate(whole_start, whole_end, agents)
 
