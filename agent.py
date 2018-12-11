624838af8b3d177db91aa357e41d9e1aa936c2a6
Bradley J. Baker <bakerb15@msu.edu>
2018-12-10 20:01:11 -0500

    housekeeping and simulation results for 20 agents

--- agent.py
+++ agent.py
@@ -5,6 +5,8 @@ import random
 #last day in data 2018-11-01
 CURRENT_DATE = pandas.datetime(2016, 11, 28) # last day in training set
 
+MAX_KNOWLEDGE = 10
+
 class Agent(object):
 
     def __init__(self, agent_name, starting_dollars, market_simulation, model):
@@ -14,19 +16,29 @@ class Agent(object):
         self.market_simulation = market_simulation
         self.market_simulation.add_me(self)
         self.knowledge = deque()
-        self.MAX_KNOWLEDGE = 50
+
         self.last_company_price = {}
         self.last_market_index_price = {}
         self.model = model
         self.avg_signal = 0
+        self.steps = 0
 
     def observe(self, info):
+        self.steps += 1
         current_signal = 0
+        if len(self.knowledge) > 0:
+            discount = 1/len(self.knowledge)
+        else:
+            discount = 0
+        count = 1
         for item in self.knowledge:
-            current_signal += item
+            current_signal += (count * discount * item)
+            count += 1
+        if len(self.knowledge) > 0:
             current_signal = current_signal/len(self.knowledge)
-        self.act(current_signal)
-        if len(self.knowledge) < self.MAX_KNOWLEDGE:
+        if self.steps >= MAX_KNOWLEDGE:
+            self.act(current_signal)
+        if len(self.knowledge) < MAX_KNOWLEDGE:
             self.knowledge.append(self.model.predict(info, batch_size=1)[0])
         else:
             self.knowledge.popleft()
@@ -46,7 +58,7 @@ class Agent(object):
             self.sell()
 
     def buy(self):
-        percent_to_spend = random.uniform(0.0, 0.75)
+        percent_to_spend =  random.uniform(0.0, 1.0)
         dollars_to_market = (percent_to_spend * self.dollars)
         self.dollars -= dollars_to_market
         new_shares, change  = self.market_simulation.purchase(dollars_to_market)
@@ -54,7 +66,7 @@ class Agent(object):
         self.dollars += change
 
     def sell(self):
-        percent_to_sell = random.uniform(0.0, 0.75)
+        percent_to_sell = random.uniform(0.0, 1.0)
         shares_to_sell = int(percent_to_sell * self.shares)
         if shares_to_sell > 0:
             self.dollars += self.market_simulation.sell_holdings(int(percent_to_sell*self.shares))
@@ -80,11 +92,14 @@ class PassiveAgent(object):
         self.model = model
         self.avg_signal = 0
         self.has_purchased = False
+        self.steps = 0
 
     def observe(self, info):
-        if self.has_purchased is False:
-            self.buy()
-            self.has_purchased = True
+        self.steps += 1
+        if self.steps >= MAX_KNOWLEDGE:
+            if self.has_purchased is False:
+                self.buy()
+                self.has_purchased = True
 
 
 
--- simulate_agent.py
+++ simulate_agent.py
@@ -3,8 +3,14 @@ from agent import Agent, PassiveAgent
 import pandas
 from keras.models import model_from_json
 
+#the name of the model used for the active agent
+#make sure .h5 and .json are named the same
 model = 'model_final4'
+
+
 TRANSACTION_COST = 10.0
+AGENTS_TO_SIMULATE = 20
+STARTING_DOLLARS = 100000.00
 print('Creating market')
 market = Market(TRANSACTION_COST)
 print('Market data loaded')
@@ -16,16 +22,24 @@ market.create_simulation_SandPonly(whole_start, whole_end)
 loaded_model_json = None
 with open('./{}.json'.format(model), 'r') as json_file:
     loaded_model_json = json_file.read()
-loaded_model = model_from_json(loaded_model_json)
-# load weights into new model
-loaded_model.load_weights('./{}.h5'.format(model))
-print("Loaded model from disk")
-active_agent = Agent('bob', 100000.00, market, loaded_model)
-passive_agent = PassiveAgent('passive', 100000.00, market, loaded_model)
-with open('simulation_results_{}_act_before_signal.txt'.format(model), 'w') as port_writer:
-    port_writer.write('Passive_Agent_Net_Worth, Passive_Agent_Shares,  Active_Agent_Net_Worth, Active_Agent_Shares\n')
+
+active_agents = []
+for i in range(AGENTS_TO_SIMULATE):
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights('./{}.h5'.format(model))
+    active_agents.append(Agent('active_{}'.format(str(i)), STARTING_DOLLARS, market, loaded_model))
+passive_agent = PassiveAgent('passive', STARTING_DOLLARS,  market, loaded_model)
+with open('simulation_results_20agents_{}.csv'.format(model), 'w') as port_writer:
+    port_writer.write('Passive_Agent_Net_Worth,\t Passive_Agent_Shares,\t')
+    for agent in active_agents:
+        port_writer.write('{},\t {},\t '.format(agent.name + 'Net_Worth', agent.name + 'Shares'))
+    port_writer.write('\n')
     in_progress = True
     while in_progress:
         in_progress = market.step()
-        port_writer.write('{},\t {},\t {},\t {}\n'.format(passive_agent.net_worth(), passive_agent.shares, active_agent.net_worth(), active_agent.shares))
+        port_writer.write('{},\t {},\t '.format(passive_agent.net_worth(), passive_agent.shares))
+        for agent in active_agents:
+            port_writer.write('{},\t {},\t '.format(agent.net_worth(), agent.shares))
+        port_writer.write('\n')
 
